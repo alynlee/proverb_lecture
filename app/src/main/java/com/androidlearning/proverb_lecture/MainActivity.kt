8095e6078b1f0259e2db5d40b3ccd060c66b8083
alyn <howerll12@gmail.com>
2021-12-17 00:46:34 +0900

    just learn listview string

--- app/src/main/java/com/androidlearning/proverb_lecture/MainActivity.kt
+++ app/src/main/java/com/androidlearning/proverb_lecture/MainActivity.kt
@@ -2,10 +2,25 @@ package com.androidlearning.proverb_lecture
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val list_item = mutableListOf<String>()
+
+        list_item.add("a")
+        list_item.add("b")
+        list_item.add("c")
+
+        val listview = findViewById<ListView>(R.id.mainListView)
+
+        val listAdapter = ListViewAdapter(list_item)
+        listview.adapter = listAdapter
+
     }
 }
